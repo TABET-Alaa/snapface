99e9e4bfb94e1c2cfd34891c64c52b1cc6f88044
Alaaddine-ta <alae.tabet02@gmail.com>
2025-01-26 18:47:10 +0100

    add personnalised attribut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,16 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+   mySnap! : FaceSnap
+
+  ngOnInit(): void {
+      this.mySnap = new FaceSnap(
+        'Slims',
+        'Petite description ',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10,  
+      )
+  }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,17 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
+  
+  snapButtonText!: string;
+  userHasSnapped!: boolean
 
   ngOnInit(): void {
-      this.title = 'Slims';
-      this.description = 'Petite description ';
-      this.createdAt = new Date();
-      this.snaps = 0;
-      this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+      this.snapButtonText = "Oh Snap !";
+      this.userHasSnapped = false;
   }
+
+   onSnap() :void{
+    if(this.userHasSnapped){
+     this.unSnap()
+    }else{
+      this.snap()
+    }
+    
+   }
+
+   unSnap(){
+    this.faceSnap.removeSnap();
+    this.snapButtonText = "Oh Snap!";
+    this.userHasSnapped = false;
+   }
+
+   snap() {
+    this.faceSnap.addSnap();
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
+}
+
 }
