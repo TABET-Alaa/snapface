dae2912b26312d27b815238fd3d8fc8c52a8aab4
Alaaddine-ta <alae.tabet02@gmail.com>
2025-01-28 12:45:57 +0100

    la directive NgStyle

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
-  imports: [],
+  imports: [NgStyle ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
